91dca3d5e14916f225701140dfcf0e37de742ba5
Marschall-dev <maxarttm@gmail.com>
2020-11-02 15:31:30 +0100

    Added Example DHCP Discover Project

---
+++ CSharp DHCP Attack/DHCP Discover/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DHCP_Discover
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            IPAddress serverAddr = IPAddress.Parse("192.168.178.1");
+
+            IPEndPoint endPoint = new IPEndPoint(serverAddr, 67);
+
+            byte[] send_buffer = dhcpPacket();
+
+            if (sock.SendTo(send_buffer, endPoint) != 0)
+            {
+                Console.WriteLine("Send successful");
+            }
+
+            Console.Read();
+        }
+       
+        private static byte[] dhcpPacket()
+        {
+            Random rand = new Random();
+            byte[] _tempTransactionId = new byte[4];
+            rand.NextBytes(_tempTransactionId);
+
+            DhcpDiscoveryPacket dhcpDiscoveryPacket = new DhcpDiscoveryPacket()
+            {
+                transactionID = _tempTransactionId,
+            };
+
+            return dhcpDiscoveryPacket.buildPacket();
+        }
+    }
+}
